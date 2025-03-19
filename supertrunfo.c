d6fbcb7b73355517fee4722316718960144a4432
Ewerton2025 <ewertondossantosramos@gmail.com>
2025-03-19 00:09:37 +0000

    printf supertrunfo

---
+++ supertrunfo.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+int main(){
+
+// cadastro carta 1 Super Trunfo em C;
+
+char Estado[100] = "B"; 
+char cidade[50];   
+float Area;
+char codigodacarta[4] = "A01"; 
+int populacao;
+float PIB;
+int numerodepontosturisticos;
+
+
+printf("Qual Estado:\n");  
+scanf("%s", Estado); 
+
+printf("nome da cidade: \n");
+scanf("%s", cidade);
+
+printf("digite quantos metros Area:\n");
+scanf("%f", &Area);
+
+printf("codigo da carta\n");
+scanf("%s", codigodacarta);
+
+printf("digite o numero da  populacao:\n");
+scanf("%d", &populacao);
+
+printf("digite o PIB:\n"),
+scanf("%f", &PIB);
+
+printf("Quantos pontos turisticos\n");
+scanf("%d", &numerodepontosturisticos);
+
+printf("Estado:%s\n", Estado);
+printf("cidade:%s\n", cidade);
+printf("Area:%f\n", Area);
+printf("codigo da carta:%s\n", codigodacarta);
+printf("populacao:%d\n", populacao);
+printf("PIB:%f\n", PIB);
+printf("Numero de pontos turisticos:%d\n", numerodepontosturisticos);
+
+// cadastro carta 2 Super Trunfo em C;
+
+char Estado2[100] = "A";
+char cidade2[50];   
+float Area2;
+char codigodacarta2[4] = "A02";
+int populacao2;
+float PIB2;
+int numerodepontosturisticos2;
+
+printf("Qual Estado:\n");  
+scanf("%s", Estado); 
+
+printf("nome da cidade: \n");
+scanf("%s", cidade);
+
+printf("digite quantos metros Area:\n");
+scanf("%f", &Area);
+
+printf("codigo da carta\n");
+scanf("%s", codigodacarta);
+
+printf("digite o numero da  populacao:\n");
+scanf("%d", &populacao);
+
+printf("digite o PIB:\n"),
+scanf("%f", &PIB);
+
+printf("Quantos pontos turisticos\n");
+scanf("%d", &numerodepontosturisticos);
+
+printf("Estado:%s\n", Estado);
+printf("cidade:%s\n", cidade);
+printf("Area:%f\n", Area);
+printf("codigo da carta:%s\n", codigodacarta);
+printf("populacao:%d\n", populacao);
+printf("PIB:%f\n", PIB);
+printf("Numero de pontos turisticos:%d\n", numerodepontosturisticos);
+
+
+
+
+return 0;
+
+
+
+
+
+}
